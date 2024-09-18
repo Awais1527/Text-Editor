d185c60bb8936bd9f4f38d4ba25809284b7fcb54
Muhammad Awais <awaisikram10@gmail.com>
2024-09-18 17:34:40 +0500

    Updates

---
+++ precache-manifest.ebc20334b939861199a4786afaf3c12e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c8edecf3f97d3b12699967dbcb8d8e",
+    "url": "/Text-Editor/index.html"
+  },
+  {
+    "revision": "51415ea0db87d89a85c1",
+    "url": "/Text-Editor/static/css/main.0c76b7a2.chunk.css"
+  },
+  {
+    "revision": "15bb42d909c7691a4a39",
+    "url": "/Text-Editor/static/js/2.ab943a0d.chunk.js"
+  },
+  {
+    "revision": "97b5938ef990e58844f0cff8514cc372",
+    "url": "/Text-Editor/static/js/2.ab943a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7427fe9bee336e9698de",
+    "url": "/Text-Editor/static/js/3.4776b16b.chunk.js"
+  },
+  {
+    "revision": "51415ea0db87d89a85c1",
+    "url": "/Text-Editor/static/js/main.3f5d395e.chunk.js"
+  },
+  {
+    "revision": "023a4c5b62745e1fd332",
+    "url": "/Text-Editor/static/js/runtime-main.ced2e579.js"
+  }
+]);
